ef63d532174ebd65cdfa76ac0c6af3bc8934caf8
Aron Wedam <arwedam@edu.aau.at>
2021-04-27 17:12:25 +0200

    GameBoard v1.0

--- CluedoApp/core/src/com/cluedo/game/GameClass.java
+++ CluedoApp/core/src/com/cluedo/game/GameClass.java
@@ -1,30 +1,104 @@
 package com.cluedo.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class GameClass extends ApplicationAdapter {
+public class GameClass extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	Texture img;
+
+	private OrthogonalTiledMapRenderer renderer;
+	private OrthographicCamera camera;
+
+	private float unitScale = 1/32f;
+
+	private TiledMap map;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		map = new TmxMapLoader().load("maps/map1.tmx");
+		renderer = new OrthogonalTiledMapRenderer(map);
+
+		Gdx.input.setInputProcessor(this);
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(true, 800, 400);
+		camera.rotate(270);
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		renderer.setView(camera);
+		renderer.render();
+		camera.update();
 	}
-	
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		float x = Gdx.input.getDeltaX();
+		float y = Gdx.input.getDeltaY();
+
+		camera.translate(-y,x);
+		camera.update();
+		return true;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(float amountX, float amountY) {
+		return false;
+	}
+
 	@Override
 	public void dispose () {
+		map.dispose();
 		batch.dispose();
 		img.dispose();
 	}
