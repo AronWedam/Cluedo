0bbad8cf63da7eb6d6269c5e2d7aa8f0c5680cca
sapschernig <sapschernig@edu.aau.at>
2021-04-29 14:01:11 +0200

    created @Spielregeln

---
+++ CluedoApp/core/src/com/cluedo/game/MainMenuScreen.java
@@ -0,0 +1,55 @@
+package com.cluedo.game;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+
+public class MainMenuScreen implements Screen {
+
+    private SpriteBatch batch;
+    protected Stage stage;
+    private OrthographicCamera camera;
+
+    public MainMenuScreen() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
