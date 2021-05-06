8340e960cbca5ab2a55c1ecd9dd0316989b79f7f
sapschernig <sapschernig@edu.aau.at>
2021-05-06 15:37:16 +0200

    implemented @RulesScreen

--- CluedoApp/core/src/com/cluedo/game/RulesScreen.java
+++ CluedoApp/core/src/com/cluedo/game/RulesScreen.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -17,6 +20,7 @@ public class RulesScreen implements Screen {
     protected Stage stage;
     private Viewport viewport;
     private OrthographicCamera camera;
+    BitmapFont font = new BitmapFont();
 
     private TextureAtlas atlas;
     protected Skin skin;
@@ -40,6 +44,24 @@ public class RulesScreen implements Screen {
 
     @Override
     public void show() {
+        Label title = new Label("Rules", skin, "big-black");
+        title.setAlignment(Align.center);
+        title.setY(780);
+        title.setWidth(200);
+        stage.addActor(title);
+        String text= "Go from room to room with your detective colleagues and interpret the clues correctly " +
+                "until one of you manages to solve the murder case. In each room, you suspect all players again, " +
+                "until the circle of suspects shrink" +
+                "You can use the dice to determine who is allowed to start. " +
+                "Whoever rolls the highest value with both dice may start." +'\n'+
+                "Whoever’s turn has to complete three phases one after the other:" +'\n' +
+                "1. Roll the dice. Move and enter a Room when possible" +"\n" +
+                "2. Question your teammates and collect clues"+"\n" +
+                "3. Cross the clues you received off on the notepad and end your turn";
+
+        batch.begin();
+        font.draw(batch, text, 20,600);
+        batch.end();
 
     }
 
@@ -78,5 +100,6 @@ public class RulesScreen implements Screen {
     public void dispose() {
         skin.dispose();
         atlas.dispose();
+        font.dispose();
     }
 }
