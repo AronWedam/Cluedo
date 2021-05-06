c63bab4f1ea0e651b188d5abca166cbaf749477e
Nejc Dolinšek <nejc.dolinsek45@gmail.com>
2021-05-06 13:52:18 +0200

    deleted my GameClass so its not in conflict

--- CluedoApp/core/src/com/cluedo/game/GameClass.java
+++ CluedoApp/core/src/com/cluedo/game/GameClass.java
@@ -10,38 +10,5 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 //Test Nejc Branch
 public class GameClass extends Game {
-	SpriteBatch batch;
-	public BitmapFont font;
-	Texture img;
 
-	private StartScreen startScreen;
-
-
-	//when we start the game, this is the first screen that shows
-	//Source: https://gamedev.stackexchange.com/questions/75324/android-libgdx-game-choosing-starting-activity
-	@Override
-	public void create () {
-		this.setStartScreen();
-	}
-
-	//Method for setting the Startscreen
-	public void setStartScreen(){
-		startScreen = new StartScreen();
-		setScreen(startScreen);
-	}
-
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
