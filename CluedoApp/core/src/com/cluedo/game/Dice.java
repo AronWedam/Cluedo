fb1db95b764f5914d5360f581b0d55f52226f1bb
sapschernig <sapschernig@edu.aau.at>
2021-05-13 14:41:03 +0200

    made dice spirteSheet transparent

--- CluedoApp/core/src/com/cluedo/game/Dice.java
+++ CluedoApp/core/src/com/cluedo/game/Dice.java
@@ -1,13 +1,20 @@
 package com.cluedo.game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import java.util.Random;
 
 
-public class Dice {
+public class Dice implements Screen {
 
     int sum=0;
     int numberDice=1;
     int sideDice=6;
     Random random=new Random();
+    SpriteBatch batch;
+    Texture img;
+
 
     public Dice(int num, int sideNum) {
         this.numberDice = num;
@@ -33,4 +40,40 @@ public class Dice {
     }
 
 
+    @Override
+    public void show() {
+        batch=new SpriteBatch();
+        img=new Texture("dice_transparent.png");
+        
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
