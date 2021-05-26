ba3280d14b094f35e3188aabeee04fdf6ecf34c8
mirako <mirako@edu.aau.at>
2021-05-26 12:52:46 +0200

    Changing the design of the notebook

--- CluedoApp/core/src/com/cluedo/game/Notebook.java
+++ CluedoApp/core/src/com/cluedo/game/Notebook.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.Align;
 
 public class Notebook {
@@ -14,54 +15,82 @@ public class Notebook {
     public Table table;
     private ScrollPane pane;
     final Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+    private Label notebookText;
+    private Label handCardText;
+    private Label shownCardText;
+    private Label markText;
+    private Label suspectsText;
+    private Label weaponsText;
+    private Label roomsText;
+    private TextButton btnDice;
+    private TextButton btnAccusation;
+    private TextButton btnHelp;
+
+
 
     public Notebook() {
         this.table = new Table(skin);
         this.table.defaults().padLeft(5).align(Align.left);
         this.pane = new ScrollPane(this.table, skin);
 
-        this.table.add(new Label("Notebook: ", skin));
-        this.table.row();
-        this.table.add(new Label("(your cards in hand)", skin, "default"));
-        this.table.row();
-        this.table.add(new Label("when shown a card,", skin, "default"));
-        this.table.row();
-        this.table.add(new Label("you can mark it off below!", skin, "default"));
 
+        this.table.add(notebookText = new Label("Notebook: ", skin));
+        notebookText.setFontScale(5
+                , 5);
         this.table.row();
+
+        this.table.add(handCardText = new Label("(your cards in hand)", skin, "default"));
+        handCardText.setFontScale(3, 3);
+        this.table.row();
+        //this.table.row();
+       // this.table.row();
+
+        this.table.add(shownCardText = new Label("when shown a card,", skin, "default"));
+        shownCardText.setFontScale(3, 3);
+        this.table.row();
+
+
+        this.table.add(markText = new Label("you can mark it off below!", skin, "default"));
+        markText.setFontScale(3, 3);
+        this.table.row();
+
+
         this.table.add(new Label("", skin));
         this.table.row();
-        this.table.add(new Label("SUSPECTS", skin, "default"));
 
 
+        this.table.add(suspectsText = new Label("SUSPECTS", skin, "default"));
+        suspectsText.setFontScale(4, 4);
         this.table.row();
+
+
         this.table.add(new Label("", skin));
         this.table.row();
-        this.table.add(new Label("WEAPONS", skin, "default"));
-
-
-
+        this.table.add(weaponsText = new Label("WEAPONS", skin, "default"));
+        weaponsText.setFontScale(4, 4);
         this.table.row();
+
+
         this.table.add(new Label("", skin));
         this.table.row();
-        this.table.add(new Label("ROOMS",skin, "default"));
 
+        this.table.add(roomsText = new Label("ROOMS",skin, "default"));
+        roomsText.setFontScale(4, 4);
         this.table.row();
-        TextButton btnDice;
+
+
         this.table.add(btnDice = new TextButton("Dice", skin, "default"));
         btnDice.setSize(20, 20);
         this.table.row();
 
         this.table.row();
-        TextButton btnAccusation;
-        this.table.add(btnDice = new TextButton("Accusation", skin, "default"));
-        btnDice.setSize(20, 20);
+        this.table.add(btnAccusation = new TextButton("Accusation", skin, "default"));
+        btnAccusation.setSize(20, 20);
         this.table.row();
 
         this.table.row();
-        TextButton btnHelp;
-        this.table.add(btnDice = new TextButton("Help", skin, "default"));
-        btnDice.setSize(20, 20);
+        this.table.add(btnHelp = new TextButton("Help", skin, "default"));
+        btnHelp.setSize(20, 20);
         this.table.row();
 
 
