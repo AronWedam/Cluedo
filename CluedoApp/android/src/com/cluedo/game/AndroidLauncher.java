f7785bf2c9ed923437a81e18eb1bfdc6214944db
sapschernig <sapschernig@edu.aau.at>
2021-06-03 13:23:53 +0200

    changes for Motion Control in AndroidLauncher onCreate method

--- CluedoApp/android/src/com/cluedo/game/AndroidLauncher.java
+++ CluedoApp/android/src/com/cluedo/game/AndroidLauncher.java
@@ -5,11 +5,21 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+//https://gamefromscratch.com/libgdx-tutorial-6-motion-controls/
+
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MainScreen(), config);
+
+		//config.useGL20=true;
+
+		//enable Accelerometer and Compass
+		config.useAccelerometer=true;
+		config.useCompass=true;
+
+		//initialize(new MotionDemo(), config);
 	}
 }
