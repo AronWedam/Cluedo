4e0254181d0c6a02189d641a6a47d501a154b6ac
mirako <mirako@edu.aau.at>
2021-06-08 16:36:46 +0200

    Small changes

--- CluedoApp/core/src/com/cluedo/game/AccusationScreen.java
+++ CluedoApp/core/src/com/cluedo/game/AccusationScreen.java
@@ -96,7 +96,6 @@ public class AccusationScreen implements Screen {
         mainTable.row().colspan(2);
 
         mainTable.add(cBMissScarlett);
-        //cBMissScarlett.getLabel().setFontScale(CB_SCALING_X, CB_SCALING_Y);
         mainTable.row();
 
         mainTable.add(cBColonelMustard);
@@ -112,7 +111,6 @@ public class AccusationScreen implements Screen {
         mainTable.row();
 
         mainTable.add(cBProfessorPlum);
-        //cBProfessorPlum.getLabel().setFontScale(CB_SCALING_X, CB_SCALING_Y);
         mainTable.row();
 
         mainTable.add(mainBtn).size(100, 50).align(Align.left);
