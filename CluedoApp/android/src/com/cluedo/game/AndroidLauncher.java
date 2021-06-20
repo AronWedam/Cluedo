6bbc39199e60c20aa9c430f5607be780ae8a1f85
sapschernig <sapschernig@edu.aau.at>
2021-06-20 19:28:11 +0200

    enabled compass and accelerometer for motion detection in @AndroidLauncher

--- CluedoApp/android/src/com/cluedo/game/AndroidLauncher.java
+++ CluedoApp/android/src/com/cluedo/game/AndroidLauncher.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+//in order to implement Motion detection - tutorial used - https://gamefromscratch.com/libgdx-tutorial-6-motion-controls/
+
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		//config.useGL20=true;
+
+		//tell to enable compass and accelerometer
+		config.useCompass=true;
+		config.useAccelerometer=true;
 		initialize(new MainScreen(), config);
 	}
 }
